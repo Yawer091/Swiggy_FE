99b3c1ddead41788c0792ed72e4e067b0be2c04c
Yawer091 <yaweratique90@gmail.com>
2024-05-02 22:32:31 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./Pages/Auth";
 import Feed from "./Pages/Feed";
 import Post from "./Pages/Post";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/feed" element={<Feed />} />
-      <Route path="/post/:postId" element={<Post />} />{" "}
-      <Route path="/" element={<Auth />} />
-    </Routes>
+    <>
+      <Auth />
+    </>
   );
 }
 
